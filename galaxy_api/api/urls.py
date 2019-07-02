3d58374aff50aaa52be3b50fed3f217137c2fbec
Adrian Likins <alikins@redhat.com>
2019-07-02 15:45:58 -0400

    rm redundant 'api' path names

--- galaxy_api/api/urls.py
+++ galaxy_api/api/urls.py
@@ -8,11 +8,7 @@ prefix = settings.API_PATH_PREFIX
 app_name = 'api'
 urlpatterns = [
     re_path(r'^$', ApiRootView.as_view(), name='api_root_view'),
-    # path
-    # path('v1/', include('galaxy_api.api.v1.urls'),
-    #      name='v1'),
-    # path('v2/', include('galaxy_api.api.v2.urls'),
-    #      name='v2'),
+
     path('v1/', include('galaxy_api.api.v1.urls',
                         namespace='v1'
                         )),
