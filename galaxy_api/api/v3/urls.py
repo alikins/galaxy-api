c3390c84a19ba81ef3708749177bbd03930c1b15
Adrian Likins <alikins@redhat.com>
2019-07-11 17:39:52 -0400

    urls cleanup

--- galaxy_api/api/v3/urls.py
+++ galaxy_api/api/v3/urls.py
@@ -23,12 +23,12 @@ app_name = 'api'
 urlpatterns = [
     path('test', TestView.as_view(), name='test'),
 
-    path('namespaces', NamespaceList.as_view(), name='namespace_list'),
+    path('namespaces/', NamespaceList.as_view(), name='namespace_list'),
     path('namespaces/<int:pk>/',
          NamespaceDetail.as_view(),
          name='namespace_detail'),
 
-    path('users',
+    path('users/',
          UserList.as_view(),
          name='user_list'),
 
@@ -43,25 +43,15 @@ urlpatterns = [
          CollectionListView.as_view(),
          name='collection-list'),
 
-    # path('collections/<int:pk>/',
-    #      CollectionDetailView.as_view(),
-    #      name='collection-detail'),
-
     path('collections/<str:namespace>/<str:name>/',
          CollectionDetailView.as_view(),
          name='collection-detail'),
 
-    # path('collection-versions/<int:version_pk>/',
-    #      VersionDetailView.as_view(),
-    #      name='version-detail'),
     # path('collections/<str:namespace>/<str:name>/versions/<str:version>/',
     #      VersionDetailView.as_view(),
     #      name='version-detail'),
 
     # # Collection Version list URLs
-    # path('collections/<int:pk>/versions/',
-    #      VersionListView.as_view(),
-    #      name='version-list'),
     # path('collections/<str:namespace>/<str:name>/versions/',
     #      VersionListView.as_view(),
     #      name='version-list'),
