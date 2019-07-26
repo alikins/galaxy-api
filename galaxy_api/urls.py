f7883c4bf4dcbfbeca9a55b2ce3f959eda78aaba
Adrian Likins <alikins@redhat.com>
2019-07-26 16:03:17 -0400

    Enable /openapi url for openapi spec gen

--- galaxy_api/urls.py
+++ galaxy_api/urls.py
@@ -3,8 +3,14 @@
 from django.conf import settings
 from django.urls import include, path
 
+from rest_framework.schemas import get_schema_view
 
 api_prefix = settings.API_PATH_PREFIX.strip('/')
 urlpatterns = [
     path(f'{api_prefix}/', include('galaxy_api.api.urls', namespace='api')),
+
+    path('openapi',
+         get_schema_view(title="Your Project",
+                         description="API for all things …"),
+         name='openapi-schema'),
 ]
