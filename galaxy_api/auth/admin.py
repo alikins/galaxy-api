a1f0aca89acec23cde86f877953a029ea778e013
Adrian Likins <alikins@redhat.com>
2019-07-30 11:57:36 -0400

    Use django.contrib.auth.admin.UserAdmin

--- galaxy_api/auth/admin.py
+++ galaxy_api/auth/admin.py
@@ -1,20 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from galaxy_api.auth import models
 
 
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('__str__',
-                    'id',
-                    'username',
-                    'first_name',
-                    'last_name',
-                    'email')
-
-    list_filter = ('is_superuser',
-                   'is_staff')
-
-    search_fields = ('username',
-                     'first_name',
-                     'last_name',
-                     'email')
+class UserAdmin(BaseUserAdmin):
+    pass
