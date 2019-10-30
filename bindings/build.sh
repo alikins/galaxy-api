9a90ab59f1323399b44428788e5a87e274353796
Adrian Likins <alikins@redhat.com>
2019-10-30 13:27:19 -0400

    Use the templates in build.sh

--- bindings/build.sh
+++ bindings/build.sh
@@ -4,6 +4,7 @@ bindings_dir=$(dirname $(readlink -f "$0"))
 
 docker run --rm -u "$(id -u)" -v "${bindings_dir}:/local" \
   openapitools/openapi-generator-cli generate \
+  -t /local/openapi-templates/python \
   -i /local/openapi.yaml \
   -g python \
   -o /local/galaxy-pulp \
